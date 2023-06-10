9c0fe481c2249f1c81522c3da75378c0ab918ff7
carlachacon <kau28chacon@gmail.com>
2023-06-09 23:11:34 -0300

    feat: add cases

---
+++ carla.js
@@ -0,0 +1,27 @@
+// Crear variables a y b , luego restarlas el resultado lo colocas en el console.log()
+// Aca abajo tu codigo:
+ var a = 23
+ var b = 12
+  console.log(a-b)
+
+
+// Crear variables a y b , luego dividilas el resultado lo colocas en el console.log()
+// Aca abajo tu codigo:
+    a = 23
+      b = 3
+
+console.log(a / b)
+
+// Crear variables a y b , luego multiplicala el resultado lo colocas en el console.log()
+// Aca abajo tu codigo:
+  a = 11
+  b = 1
+
+console.log(a * b)
+
+// Crear variables a y b , luego sumalas y en final del resultado que diga "FIN". 
+// El resultado lo colocas en el console.log()
+// Aca abajo tu codigo:
+  a = 6
+  b = 5
+console.log(a + b + "fin")
---
+++ variables.js
@@ -0,0 +1,16 @@
+// Creacion de variables  ---> primero el tipo (let,var,const) luego el nombre(pepito) 
+// y por ultimo = 123 o "una palabra cualquiera" o un buleano true o false
+// Ejemplo:
+// let casa = "mi casa"
+// let soyFeliz = true
+// var dolar = 500
+// var dolarBlue = 5800
+// const pi = 3,14
+
+let auto = 644
+var tubo = "feo"
+console.log(auto+tubo)
+
+
+// console.log() para verlo en la consola al teclear Ctrl + F5
+console.log("hello")
